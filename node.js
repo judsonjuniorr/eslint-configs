6fdb8956107f3544f2e07b84e4013f8004af5087
Judson Junior <judsonjuniorr@gmail.com>
2023-08-04 10:12:34 -0300

    Added import helpers

--- node.js
+++ node.js
@@ -3,31 +3,44 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint", "eslint-plugin-import-helpers"],
   rules: {
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
         printWidth: 80,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: 'all',
-        arrowParens: 'always',
+        trailingComma: "all",
+        arrowParens: "always",
         semi: false,
       },
     ],
-    'camelcase': 'off',
-    'no-useless-constructor': 'off'
+    camelcase: "off",
+    "no-useless-constructor": "off",
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        // example configuration
+        newlinesBetween: "always",
+        groups: ["module", ["parent", "sibling", "index"]],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
   },
   settings: {
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
   },
-}
+};
